dbb7e85c998e15c53ded10b9c220ee751166d8de
dell <dell@host.docker.internal>
2020-10-25 14:31:48 +0100

    Test JUnit Jenkins

--- phones/src/test/java/com/benali/phones/PhonesApplicationTests.java
+++ phones/src/test/java/com/benali/phones/PhonesApplicationTests.java
@@ -1,11 +1,20 @@
-//package com.benali.phones;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class PhonesApplicationTests {
-//
-//
-//
-//}
+package com.benali.phones;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+class PhonesApplicationTests {
+
+	MockMvc mockMvc;
+
+	@Test
+	public void getallProductsTest() throws Exception {
+		mockMvc.perform(get("/l/listproduits/1")).andExpect(status().isOk());
+	}
+
+}
